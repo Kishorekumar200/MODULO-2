ddf78cf28f16b46f0c7c606ae72c05e48bcca79f
Kishorekumar200 <144713483+Kishorekumar200@users.noreply.github.com>
2023-10-03 12:44:33 +0530

    Add files via upload

--- foreach.js
+++ foreach.js
@@ -1,4 +1,11 @@
-var friends=["ravi","giri","don"];
-friends.forEach((item,index) => {
-    console.log(item,index)
+var friends =["raj","shami","yes","kart"];
+
+friends.forEach((item, index) => {
+     console.log(item, index);
+});
+
+var fname = "yeswanth";
+
+friends.forEach((letter, index) =>{
+    console.log(letter , index)
 });
--- matlib.js
+++ matlib.js
@@ -3,23 +3,21 @@ console.log(Math.E);
 console.log(Math.SQRT2);
 console.log(Math.SQRT1_2);
 console.log(Math.LN2);
-console.log(Math.LN10);
+console.log(Math.LOG10);
 console.log(Math.LOG2E);
 console.log(Math.LOG10E);
 
-console.log(Math.round(2.6));
+console.log(Math.round(2.6));;
 console.log(Math.ceil(4.4));
 console.log(Math.floor(4.4));
 console.log(Math.trunc(4.4));
 console.log(Math.sign(4.4));
-console.log(Math.pow(4.4));
-console.log(Math.sqrt(4.4));
+console.log(Math.pow(8, 2));
+console.log(Math.pow(64));
 console.log(Math.abs(-4.4));
-console.log(Math.max(4,44));
+console.log(Math.max(0, 2, 3, -1, 999));
+console.log(Math.min(0, 100, 2, 3, -1,999));
 
 setInterval(() => {
-    console.log(math.random());
+    console.log(Math.random());
 }, 1000);
-
-
-
